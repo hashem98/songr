4181c4e0d8537b12a0da93b849c0664bd66a798e
hashem98 <hashemsmadi98@gmail.com>
2022-03-17 16:06:00 +0300

    lab11 is Done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # songr
+## how to run application
+* Clone this repo
+* add dependencies 
+```
+dependencies {
+	implementation 'org.springframework.boot:spring-boot-starter-thymeleaf'
+	implementation 'org.springframework.boot:spring-boot-starter-web'
+	developmentOnly 'org.springframework.boot:spring-boot-devtools'
+	testImplementation 'org.springframework.boot:spring-boot-starter-test'
+}
+
+```
+* run in IDEA
+* Visit ``http://localhost:8080/``
+
+# my routs
+* ``/``
+* ``/hello``
+* ``/upperCase/{name}``
+* ``/albums``
--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,62 @@
 package com.example.songr;
 
+import com.example.songr.Model.Album;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SongrApplicationTests {
 
+	Album test = new Album();
+
+	/**
+	 * Constructor Test on null object
+	 */
 	@Test
-	void contextLoads() {
+	public void testConstructor(){
+		Assertions.assertNull(test.getArtist());
+	}
+
+	/**
+	 *  Constructor Test on not null object
+	 */
+	@Test
+	public void testConstructorWithValues(){
+		test = new Album("frank sinatra", "My way", 10, 300, "sdsd");
+		Assertions.assertNotNull(test.getArtist());
+	}
+
+	/**
+	 * Getters Testing
+	 */
+
+	@Test
+	public void testGetters(){
+		test = new Album("My way", "frank sinatra", 10, 300, "sdsd");
+		Assertions.assertEquals("My way",test.getTitle());
+		Assertions.assertEquals("frank sinatra",test.getArtist());
+		Assertions.assertEquals(10,test.getSongCount());
+		Assertions.assertEquals(300,test.getLength());
+		Assertions.assertEquals("sdsd",test.getImageUrl());
+	}
+
+	/**
+	 * Setters Testing
+	 */
+	@Test
+	public void testSetters(){
+		test = new Album();
+		test.setTitle("My way");
+		test.setArtist("frank sinatra");
+		test.setSongCount(5);
+		test.setLength(30);
+		test.setImageUrl("sdsd");
+		Assertions.assertEquals("My way",test.getTitle());
+		Assertions.assertEquals("frank sinatra",test.getArtist());
+		Assertions.assertEquals(5,test.getSongCount());
+		Assertions.assertEquals(30,test.getLength());
+		Assertions.assertEquals("sdsd",test.getImageUrl());
 	}
 
 }
