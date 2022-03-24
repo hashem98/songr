708746d472de6ec367c5760dd279f48628d1b3b7
hashem98 <hashemsmadi98@gmail.com>
2022-03-24 18:15:34 +0300

    lab is Done

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -3,9 +3,19 @@ package com.example.songr;
 import com.example.songr.Model.Album;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SongrApplicationTests {
 
 	Album test = new Album();
@@ -58,5 +68,15 @@ class SongrApplicationTests {
 		Assertions.assertEquals(30,test.getLength());
 		Assertions.assertEquals("sdsd",test.getImageUrl());
 	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello World!")));
+	}
+
 
 }
